ae0f0ca17f5cd7f76280ee7b50858ee626f64547
routebyintuition <nodeps@gmail.com>
2021-02-05 16:18:45 -0800

    vdisk list

--- pe/disks.go
+++ pe/disks.go
@@ -24,13 +24,24 @@ type DiskListResponse struct {
 	Entities []Entities `json:"entities"`
 }
 
+// DiskVirtualListRequest provides GET parameters for vdisk list
+type DiskVirtualListRequest struct {
+	Count               int    `url:"count,omitempty"`
+	FilterCriteria      string `url:"filter_criteria,omitempty"`
+	SortCriteria        string `url:"sort_criteria,omitempty"`
+	SearchString        string `url:"search_string,omitempty"`
+	SearchAttributeList string `url:"search_attribute_list,omitempty"`
+	Page                int    `url:"page,omitempty"`
+	Projection          string `url:"projection,omitempty"`
+}
+
 // DiskVirtualListResponse provides details on all virtual disks including NFS paths
 type DiskVirtualListResponse struct {
 	Metadata Metadata   `json:"metadata"`
 	Entities []Entities `json:"entities"`
 }
 
-// List makes the call to cluster list
+// List makes the call to list hardware disks
 func (dsk *DiskService) List(reqdata *DiskListRequest) (*DiskListResponse, *http.Response, error) {
 
 	u := "disks"
@@ -46,6 +57,30 @@ func (dsk *DiskService) List(reqdata *DiskListRequest) (*DiskListResponse, *http
 	}
 
 	var data *DiskListResponse
+	resp, err := dsk.client.Do(req, &data)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return data, resp, nil
+}
+
+// ListVDisk makes the call to list virtual disks
+func (dsk *DiskService) ListVDisk(reqdata *DiskVirtualListRequest) (*DiskVirtualListResponse, *http.Response, error) {
+
+	u := "virtual_disks"
+
+	u, err := addOptions(u, reqdata)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := dsk.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var data *DiskVirtualListResponse
 	resp, err := dsk.client.Do(req, &data)
 	if err != nil {
 		return nil, resp, err
